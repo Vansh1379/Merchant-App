3fee4d6c0377b41cfe6380e33a19c41961d8f558
Vansh Kalra <vanshkalra1379@gmail.com>
2024-11-30 16:46:08 +0530

    Added nothing

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,50 +1,55 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import db from "@repo/db/client";
 const app = express();
-import db from "@repo/db/client"
 
-app.post('/webhook', async (req: Request, res: Response) => {
-    const paymentImformation = {
-        // add zod 
-        // check if request is actually coming from hdfc , use a webhokk secret here 
+app.use(express.json())
+
+app.post("/hdfcWebhook", async (req, res) => {
+    // Add zod
+    //HDFC bank should ideally send us a secret 
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     };
+
     try {
         await db.$transaction([
-            db.balance.update({
+            db.balance.updateMany({
                 where: {
-                    userId: Number(paymentImformation.userId)
+                    userId: Number(paymentInformation.userId)
                 },
                 data: {
                     amount: {
-                        increment: Number(paymentImformation.amount)
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
                     }
                 }
             }),
-            db.onRampTransaction.update({
+            db.onRampTransaction.updateMany({
                 where: {
-                    token: paymentImformation.token
+                    token: paymentInformation.token
                 },
                 data: {
-                    status: "Success"
+                    status: "Success",
                 }
-            });
+            })
+        ]);
 
-
-        res.status(200).json({
-            message: "captured"
+        res.json({
+            message: "Captured"
         })
-        ])
-        } catch (err) {
-    res.status(411).json({
-        message: "Transaction failed!!"
-    })
-}
+    } catch (e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
 
 })
 
-app.listen(3000, () => {
-    console.log('Web hook is running at port 3000');
-})
-
+app.listen(3003);
